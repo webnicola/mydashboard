55ea9feda069e00469fba29ed3afa5f47b97af4b
Nicola Dimatteo <git@nicoladimatteo.it>
2021-04-09 11:57:18 +0200

    vari esempi

---
+++ src/components/TheFooter.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="footer">
+      <p>{{ logo }}</p>
+      <p>Copy...</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TheFooter',
+  props: {
+    logo: {
+        type: String,
+        default: ''
+    }
+    
+  }
+}
+</script>
+<style scoped>
+  
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -5,10 +5,13 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    news: null,
+    count: null
   },
   mutations: {
   },
   actions: {
+
   },
   modules: {
   }
